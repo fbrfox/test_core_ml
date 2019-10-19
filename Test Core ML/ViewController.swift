d93577f2f0465e31ecd5fa5d73e32ac43f20b3ec
Pedro Henrique <fbrfox@gmail.com>
2019-10-18 23:42:15 -0300

    Initial Commit

---
+++ Test Core ML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Core ML
+//
+//  Created by Flavio on 18/10/2019.
+//  Copyright © 2019 PedroHenrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Test Core MLTests/Test_Core_MLTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_Core_MLTests.swift
+//  Test Core MLTests
+//
+//  Created by Flavio on 18/10/2019.
+//  Copyright © 2019 PedroHenrique. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Core_ML
+
+class Test_Core_MLTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
